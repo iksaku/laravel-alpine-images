09abbcae413b24158e07228cdbd996eb3e3a4ca7
Jorge González <yo@jorgeglz.io>
2024-01-14 21:57:49 -0600

    Align with Sail default and use port 80 by default

--- 8.x/shared/start-server.sh
+++ 8.x/shared/start-server.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-php -d variables_order=EGPCS $APP_DIRECTORY/artisan serve --host=0.0.0.0 --port=8080
+php -d variables_order=EGPCS $APP_DIRECTORY/artisan serve --host=0.0.0.0 --port=80
